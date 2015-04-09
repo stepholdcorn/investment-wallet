a0365530046b53042b181fc6c6fbe072a0e6cb93
Steph Oldcorn <stephieoldcorn@gmail.com>
2015-04-09 16:24:03 +0100

    Allows users to enter a new ticker

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -4,4 +4,19 @@ class InvestmentsController < ApplicationController
     @investments = Investment.all
   end
 
+  def new
+    @investment = Investment.new
+  end
+
+  def create
+    Investment.create(investment_params)
+    redirect_to investments_path
+  end
+
+  private
+
+  def investment_params
+    params.require(:investment).permit(:name, :quantity)
+  end
+
 end
--- spec/features/ticker_feature_spec.rb
+++ spec/features/ticker_feature_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 feature 'tickers' do
+
   context 'no tickers have been added' do
     scenario 'displays a prompt to add a ticker' do
       visit '/investments'
       expect(page).to have_content 'Your wallet is empty'
-      expect(page).to have_button 'Add ticker'
+      expect(page).to have_link 'Add ticker'
     end
   end
 
@@ -20,4 +21,17 @@ feature 'tickers' do
       expect(page).not_to have_content 'Your wallet is empty'
     end
   end
+
+  context 'adding a ticker' do
+    scenario 'prompts user to fill in a form, then displays the new ticker' do
+      visit '/investments'
+      click_link 'Add ticker'
+      fill_in 'Name', with: 'AAPL'
+      fill_in 'Quantity', with: '100'
+      click_button 'Add'
+      expect(page).to have_content 'AAPL'
+      expect(current_path).to eq '/investments'
+    end
+  end
+
 end
